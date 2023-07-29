ce49802d53400cef467f6dedb56184d69a3674df
jemma-mg <jemmamariyageorge@gmail.com>
2023-07-29 09:52:02 +0530

    prime numbers

---
+++ prime_numbers.py
@@ -0,0 +1,18 @@
+def factors(n):
+    factors_list = []
+    for i in range(1, n+1):
+        if n%i == 0:
+            factors_list.append(i)
+    return (factors_list)
+
+##prime numbers have only 2 fators 1 and itself
+def prime(n):
+    return(factors(n) == [1,n])
+
+def check_prime(n):
+    return(len(factors(n))==2)
+
+print(prime(2))
+print(check_prime(3))
+print(prime(9))
+print(check_prime(18))
