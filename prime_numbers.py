ee0dabc4c643c18d3f508eeaa93b7e8d287209a8
jemma-mg <jemmamariyageorge@gmail.com>
2023-07-29 18:58:55 +0530

    prime numbers from 1 to 100

--- prime_numbers.py
+++ prime_numbers.py
@@ -5,7 +5,7 @@ def factors(n):
             factors_list.append(i)
     return (factors_list)
 
-##prime numbers have only 2 fators 1 and itself
+## prime numbers have only 2 fators 1 and itself
 def prime(n):
     return(factors(n) == [1,n])
 
@@ -14,5 +14,13 @@ def check_prime(n):
 
 print(prime(2))
 print(check_prime(3))
+
 print(prime(9))
 print(check_prime(18))
+
+## prime numbers from 1-100
+prime_list = []
+for i in range(1,101):
+    if prime(i):
+        prime_list.append(i)
+print(prime_list)
